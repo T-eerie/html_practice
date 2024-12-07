ff49afc5261b15f0504dba5d637ac639e2f0880d
@T-eerie <123456789+odin@users.noreply.github.com>
2024-12-07 19:01:51 +0200

    Flex box learning

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-let btn = document.getElementById('botton');
-let subject1 = document.getElementById('subject1');
-let subject2 = document.getElementById('subject2');
-let subject3 = document.getElementById('subject3');
-let subject4 = document.getElementById('subject4');
+let btn = document.getElementById('button');
+let num1 = document.getElementById('num1');
+let num2 = document.getElementById('num2');
+let num3 = document.getElementById('num3');
+let num4 = document.getElementById('num4');
 let display = document.getElementById('display');
 btn.addEventListener('click', () => {
-    //alert('We are on')
-    let average = (+subject1.value + +subject2.value + +subject3.value + +subject4.value)/4;
+     //alert('We are on')
+    let average = (+num1.value + +num2.value + +num3.value + +num4.value)/4;
     display.innerText = `The average is: ${average}`;
-})
-
-let putme = document.getElementById('putme');
-let display2 = document.getElementById('display2');
-btn.addEventListener('click', () => {
-    //alert('Still available')
-    let aggregate = (+average.value + +putme.value)/2;
-    display.innerText = `The aggregate is: ${aggregate}`;
 })
